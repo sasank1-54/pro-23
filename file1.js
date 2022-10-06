29065b0bc5b9bfba991433efd46803670850c118
sasank1-54 <114579796+sasank1-54@users.noreply.github.com>
2022-10-06 15:55:44 +0530

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,11 @@
+//While Loop
+let count = 0;
+let arr = [];
+
+while(count<5){
+    ip = prompt("Enter the Marks: ");
+    arr[count] = ip;
+    count++;
+
+}
+document.write(arr)
---
+++ file2.js
@@ -0,0 +1,11 @@
+//For Loop
+
+let val = 0;
+let arr2 = [];
+
+for(val = 0; val<5; val++){
+    ip = prompt("Enter the Marks: ");
+    arr2[val] = ip;
+
+}
+document.write("<br>"+arr2)
